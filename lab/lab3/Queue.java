9456021032eae8852038be20e523a2ea11dc32db
Maxime <mcote074@uottawa.ca>
2018-10-01 10:49:35 -0400

    add lab 3

---
+++ lab/lab3/Queue.java
@@ -0,0 +1,35 @@
+//package lab3;
+import java.util.LinkedList;
+
+public class Queue {
+
+	private LinkedList <Task>tasks;
+
+	public Queue() {
+		this.tasks = new LinkedList <Task>();
+	}
+
+
+	public void add( Task task ) {
+		this.tasks.addFirst ( task );
+	} /* add */
+
+
+	public Task remove() throws EmptyQueueException {
+		if( !this.isEmpty() ) {
+			Task    task = this.tasks.removeLast();
+
+			return task;
+		}
+		else {
+			throw new EmptyQueueException ( "Empty queue." );
+		}
+	} /* remove */
+
+
+	public boolean isEmpty() {
+		return this.tasks.isEmpty();
+	} /* isEmpty */
+
+
+}
