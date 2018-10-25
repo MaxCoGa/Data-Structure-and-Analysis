23728e42bd510a1faa2c2130b6590bec1a6aecfa
Maxime <mcote074@uottawa.ca>
2018-10-25 09:12:58 -0400

    add lab4

---
+++ lab/lab4/Entry.java
@@ -0,0 +1,34 @@
+package lab4;
+
+/**
+ * @author Lachlan
+ */
+public class Entry <K extends Comparable, V> {
+	K key;
+	V value;
+
+	/**
+	 * Returns the key stored in this entry.
+	 * @return the entry's key
+	 */
+	K getKey() {
+		return key;
+	} /* getKey */
+
+
+	/**
+	 * Returns the value stored in this entry.
+	 * @return the entry's value
+	 */
+	V getValue() {
+		return value;
+	} /* getValue */
+
+
+	public Entry( K k, V v ) {
+		key   = k;
+		value = v;
+	}
+
+
+}
